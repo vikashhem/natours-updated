cd219b886e4a60dea0b4c4fc7284f94e621d8c6a
vikashhem <vikash.hembram.55@gmail.com>
2022-11-07 01:34:14 +0530

    minor fixes1

--- utils/email.js
+++ utils/email.js
@@ -1,13 +1,13 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-// const { htmlToText } = require('html-to-text');
+// const htmlToText = require('html-to-text');
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Kurbonaliev Muhammadali <${process.env.EMAIL_FROM}>`;
+    this.from = `Vikash Hembram <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
@@ -16,13 +16,12 @@ module.exports = class Email {
       return nodemailer.createTransport({
         service: 'SendGrid',
         auth: {
-          user: process.env.SENDGRID__USERNAME,
-          pass: process.env.SENDGRID__PASSWORD,
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
         },
       });
     }
 
-    // 1) Create a transporter
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
@@ -36,14 +35,11 @@ module.exports = class Email {
   // Send the actual email
   async send(template, subject) {
     // 1) Render HTML based on a pug template
-    const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
-      {
-        firstName: this.firstName,
-        url: this.url,
-        subject,
-      }
-    );
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
 
     // 2) Define email options
     const mailOptions = {
@@ -51,7 +47,7 @@ module.exports = class Email {
       to: this.to,
       subject,
       html,
-      // text: htmlToText(html),
+      text: htmlToText.fromString(html),
     };
 
     // 3) Create a transport and send email
