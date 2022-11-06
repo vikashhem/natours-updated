5591ba6a146d4d6e2837be807f73e4f348f82b99
vikashhem <vikash.hembram.55@gmail.com>
2022-11-07 00:43:50 +0530

    login page fix

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,6 @@
 /* eslint-disable */
-
-import { showAlert } from './alerts';
-
 import axios from 'axios';
-
+import { showAlert } from './alerts';
 export const login = async (email, password) => {
   try {
     const res = await axios({
@@ -16,7 +13,7 @@ export const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Logged in successfully');
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
@@ -32,35 +29,12 @@ export const logout = async () => {
       method: 'GET',
       url: '/api/v1/users/logout',
     });
-
     if (res.data.status === 'success') {
-      location.assign('/');
-    }
-  } catch (err) {
-    showAlert('error', 'Error logging out! Try again.');
-  }
-};
-
-export const signup = async (name, email, password, passwordConfirm) => {
-  try {
-    const res = await axios({
-      method: 'POST',
-      url: '/api/v1/users/signup',
-      data: {
-        name,
-        email,
-        password,
-        passwordConfirm,
-      },
-    });
-
-    if (res.data.status === 'success') {
-      showAlert('success', 'Signed up successfully');
-      window.setImmediate(() => {
+      window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    showAlert('error', err.response.data.message);
+    showAlert('error', 'Error logging out! Try again.');
   }
 };
