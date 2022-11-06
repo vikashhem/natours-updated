764014de9e576888d18f85200d1e7f03658f741c
vikashhem <vikash.hembram.55@gmail.com>
2022-11-06 23:52:10 +0530

    Update stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
-
 import axios from 'axios';
 import { showAlert } from './alerts';
-
-const stripe = Stripe(
+const stripe = require('stripe')(
   'pk_test_51M10dOSDg3PWxEyl8DpePO2cjaLdIkCiu7owOQYpXZosyVwAdZfhOhVaR1p38wSbyira80qc8w1inGvr9mQfx8n9005tLm5lDU'
 );
 
@@ -11,11 +9,10 @@ export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    console.log(session);
 
-    // 2) Create checkout form + change credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id,
-    });
+    // 2) Create checkout form + charge credit card
+    window.location.replace(session.data.session.url);
   } catch (err) {
     console.log(err);
     showAlert('error', err);
